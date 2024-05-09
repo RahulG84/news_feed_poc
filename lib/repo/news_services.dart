35fa3b22aac1d3c46a6dc7c5ea96eca17bbc4d8b
RahulG84 <rahul4girgal@gmail.com>
2024-05-09 11:02:52 +0530

    Changes : API Error handling

--- lib/repo/news_services.dart
+++ lib/repo/news_services.dart
@@ -4,7 +4,7 @@ import 'package:news_feed_poc/model/news_data_model.dart';
 
 class ApiService {
   static Future<List<Articles>> fetchNewsData(
-      {required http.Client client, int? newsPerPage,int? page}) async {
+      {required http.Client client, int? newsPerPage, int? page}) async {
     var currDt = DateTime.now().day;
 
     String uri =
@@ -17,14 +17,17 @@ class ApiService {
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = jsonDecode(response.body);
       final List<dynamic> articles = responseData['articles'];
-      List<Articles> newsList = articles.map((article) => Articles.fromJson(article)).toList();
+      List<Articles> newsList =
+          articles.map((article) => Articles.fromJson(article)).toList();
       return newsList;
     } else if (response.statusCode == 404) {
       throw Exception('Data Not Found');
+    } else if (response.body.contains('rateLimited')) {
+      final Map<String, dynamic> res = jsonDecode(response.body);
+      String errorMessage = res['message'];
+      throw Exception(errorMessage);
     } else {
       throw Exception('Failed to load news data');
     }
   }
 }
-
-
